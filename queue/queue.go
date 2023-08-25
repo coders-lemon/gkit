548ac4c589a973210dfbf4c67d3ba127c17cbaff
yaocheng <orangezoom.cn@gmail.com>
2023-08-25 22:01:15 +0800

    :sparkles: feat: 队列实现修改
    1. 新增 queue 接口；
    2. 实现接口完成数组队列结构；
    3. 新增一些接口方法

--- queue/queue.go
+++ queue/queue.go
@@ -1,38 +1,8 @@
 package queue
 
-import "errors"
-
-var (
-	ErrQueueEmpty = errors.New("队列为空")
-)
-
-// Queue 泛型队列
-type Queue[T any] struct {
-	list []T
-}
-
-// NewQueue 初始话泛型队列
-func NewQueue[T any]() *Queue[T] {
-	list := make([]T, 0)
-	return &Queue[T]{
-		list: list,
-	}
-}
-
-// Push 向队列中添加数据
-func (queue *Queue[T]) Push(v T) T {
-	queue.list = append(queue.list, v)
-	return v
-}
-
-// Pop 获取队列中第一条数据
-// 如何这个队列为空时会返回零值及异常
-func (queue *Queue[T]) Pop() (T, error) {
-	if len(queue.list) < 1 {
-		var zero T
-		return zero, ErrQueueEmpty
-	}
-	v := queue.list[:1][0]
-	queue.list = queue.list[1:len(queue.list)]
-	return v, nil
+type Queue[T any] interface {
+	Push(v T) T
+	Pop() (T, error)
+	Empty() bool
+	Head() (T, error)
 }
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -1,12 +1,18 @@
 package queue
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	MockValues       = []int64{1, 2, 3, 5}
+	FirstValue int64 = 1
 )
 
 func TestNewQueue(t *testing.T) {
-	queue := NewQueue[int]()
+	queue := NewArrayQueue[int]()
 	queue.Push(1)
 	t.Logf("queue push data after,queue list :%+v", queue.list)
 	v, err := queue.Pop()
@@ -16,4 +22,17 @@ func TestNewQueue(t *testing.T) {
 	}
 	t.Logf("queue pop data after,queue list :%+v", queue.list)
 	assert.Equal(t, v, 1, "队列中获取的数据与导入的数据不相符")
+}
+
+func TestArrayQueue_Head(t *testing.T) {
+	queue := NewArrayQueue[int64]()
+	for _, value := range MockValues {
+		queue.Push(value)
+	}
+	head, err := queue.Head()
+	if err != nil {
+		t.Errorf("测试 Head 方法异常，异常信息为%+v", err)
+		return
+	}
+	assert.Equal(t, FirstValue, head, "获取头部数据异常")
 }
